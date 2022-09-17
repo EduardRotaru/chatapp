3efbc14b9bc13ef49c5c99f7e39d5aecdd5f2990
EduardRotaru <ioaneduardrotaru@gmail.com>
2022-09-17 17:02:20 +0300

    instantiate branch

--- API/Models/DTO/UserChatDto.cs
+++ API/Models/DTO/UserChatDto.cs
@@ -4,5 +4,7 @@ namespace API.Models.DTO
     {
         public string Username {get;set;}
         public byte[] Image { get; set; }
+
+        // here we start with connection persistance, checkout test
     }
 } 
